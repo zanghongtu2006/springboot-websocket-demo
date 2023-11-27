92280cc770089e8ad0f1c632e76e1247ca8ecd14
zanghongtu2006 <zanghongtu2006@gmail.com>
2023-11-27 17:54:22 +0800

    init project

---
+++ src/main/java/com/hongtu/project/im/ImApplication.java
@@ -0,0 +1,16 @@
+package com.hongtu.project.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hongtu
+ */
+@SpringBootApplication
+public class ImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImApplication.class, args);
+    }
+
+}
