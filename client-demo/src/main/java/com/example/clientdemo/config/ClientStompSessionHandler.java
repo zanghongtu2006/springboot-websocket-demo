421a3de6871e796889178763952829aefab92493
hongtu_zang <zanghongtu2006@gmail.com>
2024-01-03 17:19:48 +0800

    fix warnings

--- client-demo/src/main/java/com/example/clientdemo/config/ClientStompSessionHandler.java
+++ client-demo/src/main/java/com/example/clientdemo/config/ClientStompSessionHandler.java
@@ -1,23 +1,23 @@
 package com.example.clientdemo.config;
 
-import com.example.clientdemo.controller.dto.Greeting;
-import com.example.clientdemo.controller.dto.Heartbeat;
 import com.example.clientdemo.controller.dto.HelloMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.stomp.*;
+import org.springframework.lang.NonNull;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.scheduling.TaskScheduler;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
-import java.lang.reflect.Type;
 import java.util.Date;
 
 @Slf4j
 public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
     private final WebSocketStompClient stompClient;
     private final TaskScheduler taskScheduler;
-    private final String url = "ws://localhost:8080/chatserver";
+    private final static String url = "ws://localhost:8080/chatserver";
 
     @Autowired
     public ClientStompSessionHandler(WebSocketStompClient stompClient, TaskScheduler taskScheduler) {
@@ -27,7 +27,7 @@ public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
     }
 
     @Override
-    public void afterConnected(StompSession session, StompHeaders headers) {
+    public void afterConnected(StompSession session, @NonNull StompHeaders headers) {
         log.info("Client connected: headers {}", headers);
 
         String message = "one-time message from client";
@@ -36,13 +36,13 @@ public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
     }
 
     @Override
-    public void handleFrame(StompHeaders headers, Object payload) {
+    public void handleFrame(@NonNull StompHeaders headers, Object payload) {
         log.info("Client received: payload {}, headers {}", payload, headers);
     }
 
     @Override
-    public void handleException(StompSession session, StompCommand command,
-                                StompHeaders headers, byte[] payload, Throwable exception) {
+    public void handleException(@NonNull StompSession session, StompCommand command,
+                                @NonNull StompHeaders headers, @NonNull byte[] payload, Throwable exception) {
         log.error("Client error: exception {}, command {}, payload {}, headers {}",
                 exception.getMessage(), command, payload, headers);
     }
@@ -52,12 +52,12 @@ public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
         log.error("Client transport error: error {}", exception.getMessage());
         if (!session.isConnected()) {
             // 如果连接断开，启动一个重连任务
-            taskScheduler.schedule(this::reconnect, new Date(System.currentTimeMillis() + 10000));  // 10秒后尝试重连
+            taskScheduler.schedule(this::reconnect, new Date(System.currentTimeMillis() + 10000).toInstant());  // 10秒后尝试重连
         }
     }
 
     private void reconnect() {
         // 重连逻辑
-        stompClient.connect(url, this);
+        stompClient.connectAsync(url, this);
     }
 }
