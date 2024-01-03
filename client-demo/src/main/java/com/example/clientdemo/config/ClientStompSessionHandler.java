3d6b7cd5b756c62006fb4a3d1a8823535bd24608
hongtu_zang <zanghongtu2006@gmail.com>
2024-01-03 17:04:15 +0800

    clean unnecessary code

--- client-demo/src/main/java/com/example/clientdemo/config/ClientStompSessionHandler.java
+++ client-demo/src/main/java/com/example/clientdemo/config/ClientStompSessionHandler.java
@@ -23,63 +23,17 @@ public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
     public ClientStompSessionHandler(WebSocketStompClient stompClient, TaskScheduler taskScheduler) {
         this.stompClient = stompClient;
         this.taskScheduler = taskScheduler;
-        stompClient.connect(url, this);
+        reconnect();
     }
+
     @Override
     public void afterConnected(StompSession session, StompHeaders headers) {
         log.info("Client connected: headers {}", headers);
-//        session.subscribe("/topic/heartbeat", new StompFrameHandler() {
-//            @Override
-//            public Type getPayloadType(StompHeaders headers) {
-//                String contentType = headers.getFirst("content-type");
-//                if (contentType != null && contentType.equals(MimeTypeUtils.APPLICATION_JSON_VALUE)) {
-//                    return Heartbeat.class;
-//                }
-//                return Object.class;
-//            }
-//
-//            @Override
-//            public void handleFrame(StompHeaders headers, Object payload) {
-//                if (payload instanceof Heartbeat) {
-//                    Heartbeat jsonMessage = (Heartbeat) payload;
-//                    log.info(jsonMessage.toString());
-//                }
-//            }
-//        });
 
         String message = "one-time message from client";
         log.info("Client sends: {}", message);
         session.send("/app/hello", new HelloMessage(message));
-
-//        scheduleHeartbeat(session);
-
     }
-
-//    private void scheduleHeartbeat(StompSession session) {
-//        // Define the heartbeat interval in milliseconds (e.g., every 30 seconds)
-//        long heartbeatInterval = 10 * 1000; // 30 seconds
-//
-//        // Schedule a task to send heartbeat messages periodically
-//        Runnable heartbeatTask = () -> {
-//            // Send a heartbeat message to the server
-//            session.send("/app/heartbeat", System.currentTimeMillis());
-//        };
-//
-//        Thread heartbeatThread = new Thread(() -> {
-//            while (true) {
-//                try {
-//                    Thread.sleep(heartbeatInterval);
-//                    heartbeatTask.run();
-//                } catch (InterruptedException e) {
-//                    Thread.currentThread().interrupt();
-//                    break;
-//                }
-//            }
-//        });
-//
-//        // Start the heartbeat thread
-//        heartbeatThread.start();
-//    }
 
     @Override
     public void handleFrame(StompHeaders headers, Object payload) {
@@ -96,7 +50,7 @@ public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
     @Override
     public void handleTransportError(StompSession session, Throwable exception) {
         log.error("Client transport error: error {}", exception.getMessage());
-        if(!session.isConnected()) {
+        if (!session.isConnected()) {
             // 如果连接断开，启动一个重连任务
             taskScheduler.schedule(this::reconnect, new Date(System.currentTimeMillis() + 10000));  // 10秒后尝试重连
         }
